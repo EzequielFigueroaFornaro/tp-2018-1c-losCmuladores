570660ef2c94e726851c721d2ff8f6db7eb2fb77
Nicolás Goreico <ngoreico@gmail.com>
2018-04-15 15:57:55 -0300

    Creo los proyectos para cada proceso.

---
+++ planifier/src/planifier.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : planifier.c
+ Author      : losCmuladores
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Redistinto planifier.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World del planificador!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
