b72f59a065393118c0c5199e4d60de6316b013a2
Nicolás Goreico <ngoreico@gmail.com>
2018-04-15 22:50:46 -0300

    [Coordinador]Levanto puerto desde archivo de configuración usando los commons. Defino firmas de métodos de acuerdo a responsabilidades del coordinador.

--- coordinator/coordinator.c
+++ coordinator/coordinator.c
@@ -15,11 +15,11 @@ void configure_logger() {
 }
 
 //TODO llevar a commons
-int start_server(){
+int start_server(int port){
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = INADDR_ANY;
-	server_address.sin_port = htons(PORT);
+	server_address.sin_port = htons(port);
 
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -31,13 +31,14 @@ int start_server(){
 		return 1;
 	}
 
-	log_info(logger, "Listening on port %d", PORT);
+	log_info(logger, "Listening on port %d", port);
 	listen(server_socket, 100);
 
 	return server_socket;
 }
 
 //TODO llevar a commons
+//TODO levantar threads por c/conexion aceptada.
 int accept_connection(int server_socket){
 	struct sockaddr_in cli_addr;
 	socklen_t address_size;
@@ -53,12 +54,65 @@ int accept_connection(int server_socket){
 	return client_socket;
 }
 
-int main(void) {
-	puts("!!!Hello World del Coordinador!!!"); /* prints !!!Hello World!!! */
+//TODO recibir modelo de Statement. Recibir acá el resultado, o es async ?
+//Recibe solicitud del ESI.
+//1)
+void receive_statement_request(){
+
+}
+
+//Calcula a cuál mandar la instrucción.
+//2)
+//Antes de hacer esto hay que verificar que se pueda realizar la operación, sino devolver error al planificador.
+int calculate_instance_number_to_send(){
+	return 0;//TODO
+}
+
+//TODO recibir modelo de Statement. Recibir acá el resultado, o es async ?
+//3)
+int send_statement_to_instance(){
+
+	//TODO loggear respuesta de la instancia.
+}
+
+//4)
+//TODO definir el struct del resultado.
+void receive_statement_result_from_instance(){
+
+}
+
+//Devuelve el resultado al ESI.
+//5)
+void send_statement_result_to_ise(){
+
+}
+
+//Manda la configuración correspondiente a la instancia que se acaba de conectar.
+void send_configuration_to_instance(int instance_socket){
+	//TODO
+}
+
+
+//TODO terminar de levantar la cfg que sea necesaria.
+void load_configuration(char* config_file_path){
+	char* port_name = "SERVER_PORT";
+
+	log_info(logger, "Loading configuration file...");
+	t_config* config = config_create(config_file_path);
+
+	server_port = config_get_int_value(config, port_name);
+	log_info(logger, "OK.");
+}
+
+int main(int argc, char* argv[]) {
 	configure_logger();
-	int server_socket = start_server();
+	log_info(logger, "Initializing...");
+	load_configuration(argv[1]);
+	int server_socket = start_server(server_port);
 	int client_socket = accept_connection(server_socket);
-	log_info(logger, "Acepte conexión. CLient: %d", client_socket);
+
+
+	log_info(logger, "Acepte conexión. Client: %d", client_socket);
 	close(server_socket);
 	return EXIT_SUCCESS;
 }
--- coordinator/coordinator.h
+++ coordinator/coordinator.h
@@ -15,12 +15,20 @@
 #include <unistd.h> // Para close
 #include <readline/readline.h> // Para usar readline
 #include <commons/log.h>
+#include <commons/config.h>
 
+//#define PORT 8080
 
-#define PORT 8080
+int server_port;
 
 //Global variables.
 t_log * logger;
 
+
+typedef struct  {
+  int operation_id;
+  int entries_size;
+  //TODO y algo más que no entendí del enunciado.
+} __attribute__((packed)) NewInstanceHeader;
 
 #endif /* COORDINATOR_H_ */
